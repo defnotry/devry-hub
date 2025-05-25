f46d2b63b0350d8d3c8aa448b748c90d208055a2
DEVRY <143092157+defnotry@users.noreply.github.com>
2025-05-25 20:13:26 +0800

    Create premium-loader.lua

---
+++ premium-loader.lua
@@ -0,0 +1,16 @@
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+print("Welcome to devry hub! loading...")
+local experience_id = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Creator.CreatorTargetId
+
+local experiences = {
+    [3739465] = 'https://api.luarmor.net/files/v3/loaders/2146985311add4441096ab2fa045c43b.lua',  -- DH
+    [34644452] = 'https://api.luarmor.net/files/v3/loaders/3bbc2fb333727745623a2e9e69d7b8a3.lua', -- AC
+}
+
+if experiences[experience_id] then
+    loadstring(game:HttpGet(experiences[experience_id]))()
+else
+    warn("No loader found for this experience/Unsupported experience")
+end
