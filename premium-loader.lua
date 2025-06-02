a70753255899bbb875982dd6945d861558d6ccad
DEVRY <143092157+defnotry@users.noreply.github.com>
2025-06-02 20:05:24 +0800

    Update premium-loader.lua

--- premium-loader.lua
+++ premium-loader.lua
@@ -1,16 +1,22 @@
-if not game:IsLoaded() then
-    game.Loaded:Wait()
-end
-print("Welcome to devry hub! loading...")
-local experience_id = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Creator.CreatorTargetId
+repeat
+    task.wait()
+until game:IsLoaded()
 
-local experiences = {
-    [3739465] = 'https://api.luarmor.net/files/v3/loaders/2146985311add4441096ab2fa045c43b.lua',  -- DH
-    [34644452] = 'https://api.luarmor.net/files/v3/loaders/3bbc2fb333727745623a2e9e69d7b8a3.lua', -- AC
+local creator = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Creator.CreatorTargetId
+print("Welcome to devry hub! loading...")
+local games = {
+    [3739465] = 'DungeonHeroes',
+    [34644452] = 'AriseCrossover',
+    [35789249] = 'GrowAGarden'
 }
 
-if experiences[experience_id] then
-    loadstring(game:HttpGet(experiences[experience_id]))()
+if games[creator] == 'DungeonHeroes' then
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2146985311add4441096ab2fa045c43b.lua"))()
+elseif games[creator] == 'AriseCrossover' then
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/9a06c295106b79bf51a671f942cc207d.lua"))()
+elseif games[creator] == 'GrowAGarden' then
+    print("Coming Soon")
 else
-    warn("No loader found for this experience/Unsupported experience")
+    warn("Game is unsupported. Supported games: Dungeon Heroes, Arise Crossover, Grow A Garden (SOON)")
+    return
 end
