a6140fcdc8cbed4f979b8405fa8a79f23455ea56
DEVRY <143092157+defnotry@users.noreply.github.com>
2025-06-15 14:30:16 +0800

    Update premium-loader.lua

--- premium-loader.lua
+++ premium-loader.lua
@@ -5,9 +5,10 @@ print("Welcome to devry hub! loading...")
 local experience_id = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Creator.CreatorTargetId
 
 local experiences = {
-    [3739465] = 'https://api.luarmor.net/files/v3/loaders/2146985311add4441096ab2fa045c43b.lua',  -- DH
+    [3739465] = 'https://api.luarmor.net/files/v3/loaders/2146985311add4441096ab2fa045c43b.lua', -- DH
     [34644452] = 'https://api.luarmor.net/files/v3/loaders/9a06c295106b79bf51a671f942cc207d.lua', -- AC
     [35789249] = 'https://api.luarmor.net/files/v3/loaders/34a51369fb7ff2ccec7ad7595ed5df59.lua', -- GAG
+    [34063840] = "https://api.luarmor.net/files/v3/loaders/c3bbacf3e55680fd480f78095ed00d00.lua"
 }
 
 if experiences[experience_id] then
